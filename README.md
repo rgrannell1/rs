597213cd8bdb831b1d383c64f24b0754199f2f83
Róisín Grannell <r.grannell2@gmail.com>
2022-04-14 23:06:15 +0100

    added rl implementation

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# rs
+# rs: simple build system
+
+Rs is a minimal, no-dependency build-system cloned from [bs](https://github.com/labaneilers/bs).
+
+## Usage
+
+```sh
+bs ls
+bs <command> [<args>]
+bs [<default-command-args>]
+```
+
+Create a folder called `bs`, and put [executable CLI files](https://unix.stackexchange.com/questions/291217/what-does-the-argument-x-means-in-unix-regarding-permissions) written in any language in it.
+
+```
+bs
+├───build
+├───install.py
+├───run.ts
+└───test
+```
+
+Then run these commands directly:
+
+`./run.ts myargs --my-flags`
+
+Or use `rs` build to remove a few keystrokes.
+
+`rs run myargs --my-flags`
+
+## Installation
+
+```
+sudo cp rs.py /usr/bin/rs
+```
+
+## License
+
+The MIT License
+
+Copyright (c) 2022 Róisín Grannell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ rs.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import os
+from sys import argv
+import subprocess
+
+
+RS_VERSION = '1.0.0'
+BUILD_DIR = './bs'
+
+def info(message: str) -> str:
+  return message
+
+def error(message: str) -> str:
+  return f'\033[38;2;168;50;72m{message}\033[0m'
+
+def bold(message: str) -> str:
+  return f'\033[1m{message}\033[0m'
+
+
+helpfile = f"""
+rs: tiny build system
+
+Usage:
+  rs <command> [<args>]
+  rs [<default-command-args>]
+  rs ls
+
+Description:
+  Rs is a build-system that runs scripts in a folder.
+
+  1. Create a {BUILD_DIR} directory in your repository root
+
+  2. Add commands by adding executable scripts (with shebangs) with to {BUILD_DIR} with
+  the name of the command. For example
+
+  > rs publish
+
+  will call:
+
+  > {BUILD_DIR}/publish[extension]
+
+  3. Additional arguments can be passed to your command. For example,
+
+  > rs publish --stage production --verbose
+
+  will call:
+
+  > {BUILD_DIR}/publish --stage production --verbose
+
+  4. You can define a default script at {BUILD_DIR}/default[extension] which will
+  be called when the argument passed for <command> doesn't match any
+  script in {BUILD_DIR}
+
+  For example, if 'restore' does not have a matching script,
+
+  > rs restore --previous
+
+  will call
+
+  > {BUILD_DIR}/restore restore --previous
+
+See Also:
+  rs is a python-based clone of bs <https://github.com/labaneilers/bs>, with
+  a few added features.
+"""
+
+def show_rs_help():
+  for line in helpfile.splitlines():
+    if line.strip().startswith('> '):
+      print(bold(line))
+    else:
+      print(info(line))
+
+
+def list_commands():
+  for file in os.listdir(BUILD_DIR).sort():
+    execable = os.access(os.path.join(BUILD_DIR, file), os.X_OK)
+
+    if execable:
+      print(f"- {BUILD_DIR}/{file}*")
+    else:
+      print(f"- {BUILD_DIR}/{file}")
+
+
+def get_script_path(command: str) -> str:
+  match = None
+
+  for file in os.listdir(BUILD_DIR):
+    name = os.path.basename(file)
+    if name == command:
+      match = os.path.join(os.path.join(BUILD_DIR, file))
+      break
+
+    name_no_ext = Path(name).with_suffix('').stem
+    if name_no_ext == command:
+      match = os.path.join(os.path.join(BUILD_DIR, file))
+      break
+
+  if not match:
+    match = os.path.join(BUILD_DIR, 'default')
+
+  if not os.path.isfile(match):
+    if match == os.path.join(BUILD_DIR, 'default'):
+      message = f"rs: no file found for '{command}' and default-executable '{BUILD_DIR}/default' is also missing."
+
+      if command == 'list':
+        message = message + ' Did you mean "rl ls"?'
+      else:
+        message = message + ' Please create one of these files.'
+
+      print(message)
+    else:
+      print(error(f"rs: command '{command}' could not be executed as {match} does not exist"))
+
+    exit(1)
+
+  if not os.access(match, os.X_OK):
+    print(error(f"rs: command '{command}' could not be executed as {match} is not marked as executable"))
+    exit(1)
+
+  return match
+
+
+def main():
+  if len(argv) < 2:
+    show_rs_help()
+    return
+
+  command = argv[1]
+
+  if command in {'help', '--help', 'h'}:
+    show_rs_help()
+    return
+
+  if command in {'--version', 'v'}:
+    print(RS_VERSION)
+    return
+
+  if not os.path.isdir(BUILD_DIR):
+    print(error(f"rs: Expected a build-commands directory at {BUILD_DIR}, but none was found"))
+    exit(1)
+
+  if command == "ls":
+    print("rs: Available rs commands for this project (valid executables marked with *)")
+
+    list_commands()
+    exit(0)
+
+  subargs = argv[2:]
+
+  script_path = get_script_path(command)
+  instance = subprocess.run([script_path] + subargs)
+
+  exit(instance.returncode)
+
+
+main()
